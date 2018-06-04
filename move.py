1a56e1c4b5e6336837d44003ee93aee223aac059
mainidishu <mainidishu@gmail.com>
2018-06-04 23:43:55 +0530

    Fifth commit

--- move.py
+++ move.py
@@ -1,12 +1,12 @@
-#This program workss for files more than or equal to 5 bytes
+#This program works for files more than or equal to 5 bytes
 
 import os
-source='/home/deepanshu/Desktop/python/file check/'
+source='/home/deepanshu/Desktop/python/file check/'	#Source and Destination path can be changed accordingly
 dest='/home/deepanshu/Desktop/python/file check 1/'
-l=os.listdir(source)
-n=len(l)
+l=os.listdir(source)					#Get the list of directories in the path
+n=len(l)						#len() finds the number of files
 for i in range(0,n):
-	size=os.path.getsize(source + l[i])
+	size=os.path.getsize(source + l[i])		#path.getsize() finds the size of files
 	if size >= 5:
-		os.rename(source + l[i],dest + l[i])
+		os.rename(source + l[i],dest + l[i])	#rename() is used to move the file
 
